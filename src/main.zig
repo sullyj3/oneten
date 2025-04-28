4a05a7c65035aa7db14ae730e4060074527d72d0
James Sully <sullyj3@gmail.com>
2025-04-28 11:54:03 +1000

    draw row of cells

--- src/main.zig
+++ src/main.zig
@@ -8,12 +8,61 @@ const ray = @cImport({
 const sleep = std.time.sleep;
 const ns_per_s = std.time.ns_per_s;
 
+fn draw_square(x: c_int, y: c_int, side: c_int, color: ray.Color) void {
+    ray.DrawRectangle(x, y, side, side, color);
+}
+
+const CELL_SIDE = 50;
+const CELL_GAP = 7;
+const CELL_BORDER_WIDTH = 5;
+
+fn draw_cell(on: bool, x: c_int, y: c_int) void {
+    draw_square(x, y, 50, ray.VIOLET);
+    if (!on) {
+        draw_square(
+            x + CELL_BORDER_WIDTH,
+            y + CELL_BORDER_WIDTH,
+            CELL_SIDE - 2 * CELL_BORDER_WIDTH,
+            ray.RAYWHITE,
+        );
+    }
+}
+
+fn draw_cell_row(cell_states: []bool, x: c_int, y: c_int) void {
+    var x_offs: c_int = 0;
+    for (cell_states) |cell| {
+        draw_cell(cell, x + x_offs, y);
+        x_offs += CELL_SIDE + CELL_GAP;
+    }
+}
+
 pub fn main() !void {
+    // var gpa = std.heap.GeneralPurposeAllocator.init(.{}){};
+    // const alloc = gpa.alloc();
+
     const title = "oneten";
     ray.InitWindow(1080, 720, title);
     defer ray.CloseWindow();
     ray.SetTargetFPS(60);
-    ray.BeginDrawing();
-    ray.EndDrawing();
-    sleep(5 * ns_per_s);
+
+    var cells: [10]bool = undefined;
+    var toggle: bool = true;
+    inline for (0..cells.len) |i| {
+        cells[i] = toggle;
+        toggle = !toggle;
+    }
+
+    while (!ray.WindowShouldClose()) {
+        ray.BeginDrawing();
+        ray.ClearBackground(ray.RAYWHITE);
+        // draw_cell(true, 200, 200);
+        // draw_cell(false, 200 + CELL_SIDE + CELL_GAP, 200);
+        draw_cell_row(cells[0..], 150, 300);
+        ray.EndDrawing();
+
+        if (ray.IsKeyPressed(ray.KEY_ESCAPE)) {
+            std.debug.print("esc pressed", .{});
+            break;
+        }
+    }
 }
